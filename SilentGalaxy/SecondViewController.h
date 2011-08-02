2a84254faff97a0a71b5dd132c979994b2c1df15
Raúl Flores <raul@82.158.97.165.dyn.user.ono.com>
2011-08-02 10:32:07 +0200

    Initial Commit

---
+++ SilentGalaxy/SecondViewController.h
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.h
+//  SilentGalaxy
+//
+//  Created by Raúl Flores on 02/08/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SecondViewController : UIViewController {
+    
+}
+
+@end
